3cb69d4d98a370edf7f170c3474f35a5d1651d7a
Nam Vu Hoai <nam@moncow-ux.com>
2023-05-16 07:53:22 +0700

    [Update] - UI User Profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,27 @@
 var RUN = {
-  // AOS
-  aos: function () {
-    AOS.init({
-      disable: window.innerWidth < 1024,
-    });
-  },
+    // Show Menu
+    showMenu: () => {
+        $('.wrap-user').click(() => {
+            $('.user-list-option').toggle();
+        });
+    },
 
-  // Initial
-  init: function () {
-    RUN.aos();
-  },
+    // Show Modal Logout
+    showModalLogout: () => {
+        $('.btn-show-logout').click(() => {
+            $('.user-list-option').toggle();
+        });
+    },
+
+    // Initial
+    init: () => {
+        RUN.showMenu();
+        RUN.showModalLogout();
+    },
 };
 
-$(document).ready(function () {
-  setTimeout(function () {
-    RUN.init();
-    console.log(
-      "%c Template...",
-      "color: #ff0000; font-size: 16px; font-weight: 700"
-    );
-  }, 100);
+$(document).ready(() => {
+    setTimeout(() => {
+        RUN.init();
+    }, 100);
 });
