2a60da7842d191dfd93eb153d5b03febe747b760
Nam Vu Hoai <nam@moncow-ux.com>
2023-05-18 09:57:22 +0700

    [Update] - Screen C02

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,10 +13,38 @@ var RUN = {
         });
     },
 
+    // Upload Image
+    uploadImage: () => {
+        const readURL = (input, imgControlName) => {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $(imgControlName).attr('src', e.target.result);
+                };
+                reader.readAsDataURL(input.files[0]);
+            }
+        };
+
+        $('#imag').change(function () {
+            var imgControlName = '.image-preview';
+            readURL(this, imgControlName);
+        });
+
+        $('.btn-delete-image').click(function (e) {
+            e.preventDefault();
+            $('#imag').val('');
+            $('.image-preview').attr(
+                'src',
+                'https://t4.ftcdn.net/jpg/03/46/93/61/360_F_346936114_RaxE6OQogebgAWTalE1myseY1Hbb5qPM.jpg',
+            );
+        });
+    },
+
     // Initial
     init: () => {
         RUN.showMenu();
         RUN.showModalLogout();
+        RUN.uploadImage();
     },
 };
 
